5b5f657ade7d856f860b822d1131b060e50aa20b
apreet81 <apreet81@live.com>
2020-05-02 21:22:03 +0530

    Add project files.

---
+++ ExcelImport/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ExcelImport.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:FileUpload ID="FileUpload1" runat="server" />
+
+            <asp:Button ID="btnUpload" runat="server" Text="Upload"
+                OnClick="btnUpload_Click" />
+
+            <br />
+            <asp:GridView ID="GridView1" runat="server"
+                OnPageIndexChanging="PageIndexChanging" AllowPaging="true">
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ExcelImport/Default.aspx.cs
@@ -0,0 +1,77 @@
+﻿using ExcelDataReader;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Web.UI.WebControls;
+
+namespace ExcelImport
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                string FileName = Path.GetFileName(FileUpload1.PostedFile.FileName);
+
+                string Extension = Path.GetExtension(FileUpload1.PostedFile.FileName);
+
+                string FolderPath = ConfigurationManager.AppSettings["FolderPath"];
+
+                string FilePath = Server.MapPath(FolderPath + FileName);
+
+                FileUpload1.SaveAs(FilePath);
+
+                ReadExcel(FilePath);
+
+                //Import_To_Grid(FilePath, Extension);
+            }
+        }
+
+        private void ReadExcel(string FilePath)
+        {
+            using (var stream = File.Open(FilePath, FileMode.Open, FileAccess.Read))
+            {
+                // Auto-detect format, supports:
+                //  - Binary Excel files (2.0-2003 format; *.xls)
+                //  - OpenXml Excel files (2007 format; *.xlsx)
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    // Choose one of either 1 or 2:
+
+                    // 1. Use the reader methods
+                    do
+                    {
+                        while (reader.Read())
+                        {
+                            // reader.GetDouble(0);
+                        }
+                    } while (reader.NextResult());
+
+                    // 2. Use the AsDataSet extension method
+                    var result = reader.AsDataSet(new ExcelDataSetConfiguration()
+                    {
+                        ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
+                        {
+                            UseHeaderRow = true
+                        }
+                    });
+
+                    //GridView1.Caption = Path.GetFileName(FilePath);
+
+                    GridView1.DataSource = result.Tables[0];
+
+                    GridView1.DataBind();
+
+                    // The result of each spreadsheet is in result.Tables
+                }
+            }
+        }
+    }
+}
